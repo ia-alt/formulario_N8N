9a87c33be4ca74881e814a2148f04ba3adc0a3fd
Rodrigo de França <rodrigodefrancadev@gmail.com>
2025-12-11 17:26:53 -0300

    Wrap App component with ConfigProvider to set locale to Portuguese

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRouter from "./app-router";
 import "./index.css";
-import { App } from "antd";
+import { App, ConfigProvider } from "antd";
+import ptBr from "antd/locale/pt_BR";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App>
-      <AppRouter />
-    </App>
+    <ConfigProvider locale={ptBr}>
+      <App>
+        <AppRouter />
+      </App>
+    </ConfigProvider>
   </React.StrictMode>
 );
